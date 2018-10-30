731990ace7fcfef978985c5e649c4ad373425d75
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-10-29 21:01:42 -0400

    nc

--- classification.py
+++ classification.py
@@ -24,4 +24,18 @@ plt.figure()
 plt.imshow(train_images[0])
 plt.colorbar()
 plt.grid(False)
-plt.savefig("temp.png")
+
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+    plt.savefig("temp" + str(i) + ".png")
