b414626854ed08cda0ef9f02717d1b69216f42d2
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-10-29 20:45:50 -0400

    add class names

--- classification.py
+++ classification.py
@@ -1,17 +1,17 @@
-"""/Users/itn/github/tensorflow ."""
-""" TensorFlow and tf.keras. """
+"""tesorflow demo ."""
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 from tensorflow import keras
 
 """ Helper libraries """
 
-import matplotlib.pyplot as plt
-
 print(tf.__version__)
-
 
 fashion_mnist = keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
