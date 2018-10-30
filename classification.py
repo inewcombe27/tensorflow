00adc2f3286e5d592c0f0d72c541715625f06bb2
Ian Newcombe <ian.newcombe@libertymutual.com>
2018-10-29 21:06:45 -0400

    test ML

--- classification.py
+++ classification.py
@@ -38,4 +38,20 @@ for i in range(25):
     plt.grid(False)
     plt.imshow(train_images[i], cmap=plt.cm.binary)
     plt.xlabel(class_names[train_labels[i]])
-    plt.savefig("temp" + str(i) + ".png")
+plt.savefig("temp.png")
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print('Test accuracy:', test_acc)
